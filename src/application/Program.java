fc7bd67a4576da3680a22e5214b6ae643226c8d3
rivaldo-ferreira <rivaldo13@gmail.com>
2024-06-28 14:19:16 -0300

    Leitura - Versão 3 (melhor opção)

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,38 +7,23 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("***** leitura e escrita de arquivos *****".toUpperCase());
 
         String path = "D:\\A_CURSO_JAVA UDEMY\\arquivos\\arquivos_txt\\arquivo_leitura.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
         int cont = 1;
 
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
-            while(line != null){
+            while (line != null) {
                 System.out.println(cont + " - " + line);
                 line = br.readLine();
                 cont++;
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try{
-                if(br != null){
-                    br.close();
-                }
-                if(fr != null){
-                    fr.close();
-                }
-            }catch (IOException e){
-                e.printStackTrace();
-            }
         }
     }
 }
